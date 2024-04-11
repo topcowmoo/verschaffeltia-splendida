982bf0c05dd3357b497a448a64b030f3b61c3801
Salvatore <mammoliti.sam@gmail.com>
2024-04-10 20:48:09 -0400

    Vite, tailwind, google font installation and config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,4 @@
 module.exports = {
-  root: true,
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
@@ -12,10 +11,7 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': 'warn',
+    'react/prop-types': 'off',
   },
 }

143cae9bd5d3074316de7c934b74c5104938fd52
Salvatore <mammoliti.sam@gmail.com>
2024-04-11 18:15:13 -0400

    added test function and export to test

--- src/main.jsx
+++ src/main.jsx
@@ -1,45 +1,45 @@
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './index.css'
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
-import App from './App';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Error from './pages/Error';
-import Home from './pages/Home';
-import Projects from './pages/Projects';
-import Resume from './pages/Resume';
+import App from "./App";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Error from "./pages/Error";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Resume from "./pages/Resume";
 
 const router = createBrowserRouter([
-{
-  path: '/',
-  element: <App />,
-  errorElement: <Error />,
-  children: [
-    {
-      index: true,
-      element: <Home />,
-    },
-    {
-      path: '/About',
-      element: <About />,
-    },
-    {
-      path: '/Contact',
-      element: <Contact />,
-    },
-    {
-      path: '/Projects',
-      element: <Projects />,
-    },
-    {
-      path: '/Resume',
-      element: <Resume />,
-    },
-  ],
-},
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/About",
+        element: <About />,
+      },
+      {
+        path: "/Contact",
+        element: <Contact />,
+      },
+      {
+        path: "/Projects",
+        element: <Projects />,
+      },
+      {
+        path: "/Resume",
+        element: <Resume />,
+      },
+    ],
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-<RouterProvider router={router} />
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 );

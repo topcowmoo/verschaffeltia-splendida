aeb7287f229eb82234be43f96f61dc424efcf8e9
Salvatore <mammoliti.sam@gmail.com>
2024-04-11 12:52:53 -0400

    Created Error.jsx in pages folder. Add router configuration, created browserrouter instance and render routerprovider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css'
 
+import App from './App';
+import AboutMe from './pages/AboutMe';
+import Contact from './pages/Contact';
+import Error from './pages/Error';
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+
+const router = createBrowserRouter([
+{
+  path: '/',
+  element: <App />,
+  errorElement: <Error />,
+  children: [
+    {
+      index: true,
+      element: <Home />,
+    },
+    {
+      path: '/AboutMe',
+      element: <AboutMe />,
+    },
+    {
+      path: '/Contact',
+      element: <Contact />,
+    },
+    {
+      path: '/Portfolio',
+      element: <Portfolio />,
+    },
+    {
+      path: '/Resume',
+      element: <Resume />,
+    },
+  ],
+},
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+<RouterProvider router={router} />
+);
